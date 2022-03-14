bdc79422ad3dd6f0e7f53f6d26fd8064d8829168
Russell Jones <jonesrussell42@gmail.com>
2022-03-13 23:45:57 -0400

    Add contact model

---
+++ src/models/contact.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Contact extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  message: string;
+
+
+  constructor(data?: Partial<Contact>) {
+    super(data);
+  }
+}
+
+export interface ContactRelations {
+  // describe navigational properties here
+}
+
+export type ContactWithRelations = Contact & ContactRelations;
