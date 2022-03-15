4a99ac0f6944f90df57c421ff276018544f33d6f
Russell Jones <jonesrussell42@gmail.com>
2022-03-14 20:41:28 -0400

    Add pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	apps: [
+		{
+			name: 'cn-lb4.jonesrussell42.xyz',
+			script: './dist/index.js',
+			watch: './dist',
+			// Delay between restart
+			watch_delay: 1000,
+			ignore_watch: ['node_modules'],
+			env: {
+				PORT: 4000,
+				NODE_ENV: 'production',
+				MONGODB_URI: 'mongodb://mongodb.jonesrussell42.xyz:27017/portfolio-app'
+			}
+		}
+	]
+};
